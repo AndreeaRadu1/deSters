423cf67f205a016c509d4fb017ce0355ef4b5a08
Radu Andreea <andreea.radu154@gmail.com>
2022-10-25 23:09:17 +0300

    am sch branch ul

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -24,7 +24,7 @@ function Media(props: MediaProps) {
             <Skeleton animation="wave" variant="circular" width={40} height={40} />
           ) : (
             <Avatar
-              alt="Ted talk"
+              alt="Ted sdafcasfafdfsdvsdvS djwahdbj   shasdmhtalk"
               src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg"
             />
           )
@@ -63,7 +63,7 @@ function Media(props: MediaProps) {
           component="img"
           height="140"
           image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
-          alt="Nicola Sturgeon on a TED talk stage"
+          alt="Nicoldsafvdv cvdv dsacaa Sturgeon on a TED talk stage"
         />
       )}
       <CardContent>
