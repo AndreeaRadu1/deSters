588f3dc928534141dfb41d9fbb3e938d324a4d09
Radu Andreea <andreea.radu154@gmail.com>
2022-10-25 23:06:45 +0300

    commit de test

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { CircularProgress } from '@mui/material';
+import Posts from './components/Posts';
+import CreatePost from './components/CreatePost';
+import Skeleton  from './components/Skeleton';
+import './components/skeleton.css';
+
+const initialData = [
+	{
+		title: "Post title 1",
+		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices lacinia vehicula. In vel feugiat nibh. Suspendisse faucibus, magna vitae fermentum pulvinar, elit sapien elementum turpis, at pretium leo enim quis purus.`
+	},
+	{
+		title: "Post title 2",
+		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices lacinia vehicula. In vel feugiat nibh. Suspendisse faucibus, magna vitae fermentum pulvinar, elit sapien elementum turpis, at pretium leo enim quis purus.`
+	},
+	{
+		title: "Post title 3",
+		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices lacinia vehicula. In vel feugiat nibh. Suspendisse faucibus, magna vitae fermentum pulvinar, elit sapien elementum turpis, at pretium leo enim quis purus.`
+	},
+];
+
+function App() {
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setData(initialData);
+			setIsLoading(false);
+		}, 2000);
+	}, []);
+
+	return (
+		<div className="App">
+			<div style={{ width: "70%", margin: "auto" }}>
+				<CreatePost data={data} setData={setData} />
+				{isLoading ?
+				<div className="threeInARow">
+				   <Skeleton />
+				   <Skeleton />
+				   <Skeleton />
+				</div>
+				 : <Posts data={data} />}
+			</div>
+		</div>
+	);
+}
+
+export default App;
